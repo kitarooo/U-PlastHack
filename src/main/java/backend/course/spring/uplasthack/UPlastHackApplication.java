d6159b860beae8ba97420579cf37742a3b9c5ff8
azamatshauenbekov <bravlstarstop05@mail.ru>
2024-03-11 13:55:49 +0500

    Initial commit

---
+++ src/main/java/backend/course/spring/uplasthack/UPlastHackApplication.java
@@ -0,0 +1,13 @@
+package backend.course.spring.uplasthack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UPlastHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UPlastHackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/course/spring/uplasthack/UPlastHackApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.course.spring.uplasthack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UPlastHackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
